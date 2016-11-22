c5ed7ec31468914b240c9bd52977eae2e196f9d1
Matthieu Le Berre <matthieu.leberre@gmail.com>
2016-11-22 17:42:34 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# db/seeds.rb
+require 'faker'
+# Creating 100 fake restaurants
+100.times do
+  article = Article.new(
+    title:    Faker::Book.title,
+    content:  Faker::Lorem.paragraphs,
+  )
+  article.save!
+end
